0ee8e98c880c826b20cdc1727ee19f55ea0dd2e5
Toushik Banik <toushikbanikeducation@gmail.com>
2022-03-02 10:07:00 +0530

    shado

--- lib/main.dart
+++ lib/main.dart
@@ -2,170 +2,186 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp( const foods());
 }
 
+class foods extends StatelessWidget {
+  const foods({Key? key}) : super(key: key);
 
-class MyApp extends StatefulWidget {
-
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          backgroundColor: const Color.fromARGB(255, 246, 246, 242),
-            body:
-              ListView.builder(
-                //itemCount: 40,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    margin: const EdgeInsets.all(20),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            children: [
-                              TextButton(
-                                child: Stack(
-                                  clipBehavior: Clip.none,
-                                  children: [
-                                    Container(
-                                        child:  Center(
-                                          child: Container(
-                                            padding: const EdgeInsets.fromLTRB(35,70,35,10),
-                                            child: Text(
-                                              'Your Food No: ${index}',
-                                              style: const TextStyle(
-                                                fontSize: 20.0,
-                                                color: Colors.red,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                        height: 200.0,
-                                        width: 150.0,
-                                        decoration: BoxDecoration(
-                                          color: const Color.fromARGB(255, 255, 255, 255),
-                                          borderRadius: BorderRadius.circular(30),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color: Colors.blue.withOpacity(0.5),
-                                              spreadRadius: 5,
-                                              blurRadius: 7,
-                                              offset: const Offset(1.0,5.0),
-                                            )
-                                          ]
-                                        )
-                                    ),
-                                    Positioned(
-                                      left: 0,
-                                      right: 0,
-                                      top: -40,
-                                      child: Center(
-                                        child: Container(
-                                          width: 120,
-                                          height: 120,
-                                          decoration: const BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            image: DecorationImage(
-                                              image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
-                                              fit: BoxFit.fitWidth,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
+      home: Scaffold(
+        appBar: AppBar(leading: const Icon(Icons.arrow_back_ios),
+          title: const Text('Spicy Chicken'),
+          backgroundColor:  Colors.transparent,
 
-                                  ],
-                                ),
-                                onPressed: (){
-                                  print('hi1');
-                                },
-                              ),
-                              const SizedBox(
-                                height: 50.0,
-                              )
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            children: [
-                              const SizedBox(
-                                height: 50.0,
-                              ),
-                              TextButton(
-                                child: Stack(
-                                  clipBehavior: Clip.none,
-                                  children: [
-                                    Container(
-                                        child:  Center(
-                                          child: Container(
-                                            padding: const EdgeInsets.fromLTRB(35,70,35,10),
-                                            child: Text(
-                                              'Your Food No: ${index}',
-                                              style: const TextStyle(
-                                                fontSize: 20.0,
-                                                color: Colors.red,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                        height: 200.0,
-                                        width: 150.0,
-                                        decoration: BoxDecoration(
-                                            color: const Color.fromARGB(255, 255, 255, 255),
-                                            borderRadius: BorderRadius.circular(30),
-                                            boxShadow: [
-                                              BoxShadow(
-                                                color: Colors.blue.withOpacity(0.5),
-                                                spreadRadius: 5,
-                                                blurRadius: 7,
-                                                offset: const Offset(1.0,5.0),
-                                              )
-                                            ]
-                                        )
-                                    ),
-                                    Positioned(
-                                      left: 0,
-                                      right: 0,
-                                      top: -40,
-                                      child: Center(
-                                        child: Container(
-                                          width: 120,
-                                          height: 120,
-                                          decoration: const BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            image: DecorationImage(
-                                              image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
-                                              fit: BoxFit.fitWidth,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-
-                                  ],
-                                ),
-                                onPressed: (){
-                                  print('hi1');
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-          )
         ),
+        body: const foodspage(),
+      ),
     );
   }
 }
+
+class foodspage extends StatefulWidget {
+  const foodspage({Key? key}) : super(key: key);
+
+  @override
+  _foodspageState createState() => _foodspageState();
+}
+
+class _foodspageState extends State<foodspage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 249, 249, 249),
+        body: ListView.builder(
+          //itemCount: 40,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              margin: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        TextButton(
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Container(
+                                  child:  Center(
+                                    child: Container(
+                                      padding: const EdgeInsets.fromLTRB(35,70,35,10),
+                                      child: Text(
+                                        'Your Food No: ${index}',
+                                        style: const TextStyle(
+                                          fontSize: 20.0,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  height: 200.0,
+                                  width: 150.0,
+                                  decoration: BoxDecoration(
+                                      color: const Color.fromARGB(255, 255, 255, 255),
+                                      borderRadius: BorderRadius.circular(30),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: const Color.fromARGB(57, 57, 57, 10).withOpacity(0.25),
+                                          spreadRadius: 0,
+                                          blurRadius: 60,
+                                          offset: const Offset(0.0,30.0),
+                                        )
+                                      ]
+                                  )
+                              ),
+                              Positioned(
+                                left: 0,
+                                right: 0,
+                                top: -40,
+                                child: Center(
+                                  child: Container(
+                                    width: 120,
+                                    height: 120,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                        image: NetworkImage('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                            ],
+                          ),
+                          onPressed: (){
+                            print('hi1');
+                          },
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 70.0,
+                        ),
+                        TextButton(
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Container(
+                                  child:  Center(
+                                    child: Container(
+                                      padding: const EdgeInsets.fromLTRB(35,70,35,10),
+                                      child: Text(
+                                        'Your Food No: ${index}',
+                                        style: const TextStyle(
+                                          fontSize: 20.0,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  height: 200.0,
+                                  width: 150.0,
+                                  decoration: BoxDecoration(
+                                      color: const Color.fromARGB(255, 255, 255, 255),
+                                      borderRadius: BorderRadius.circular(30),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: const Color.fromARGB(57, 57, 57, 10).withOpacity(0.25),
+                                          spreadRadius: 0,
+                                          blurRadius: 60,
+                                          offset: const Offset(0.0,30.0),
+                                        )
+                                      ]
+                                  )
+                              ),
+                              Positioned(
+                                left: 0,
+                                right: 0,
+                                top: -40,
+                                child: Center(
+                                  child: Container(
+                                    width: 120,
+                                    height: 120,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                        image: NetworkImage('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                            ],
+                          ),
+                          onPressed: (){
+                            print('hi1');
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
